a5fe45722f9f6a3d3cb60789825e0a6c617edb4f
kohlsjw3656 <kohlsjw3656@gmail.com>
2020-03-03 22:20:09 -0600

    Fixed nav resizing

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var window, document;
+
+function navResize() {
+    var navItems = document.getElementsByClassName("navResize");
+    var vw = document.documentElement.clientWidth;
+
+    for (var i = 0; i < navItems.length; i++) {
+        if (vw < 768) {
+            navItems[i].setAttribute("class", "col-xs-12 navResize");
+        }
+        else {
+            navItems[i].setAttribute("class", "navResize");
+        }
+    }
+}
