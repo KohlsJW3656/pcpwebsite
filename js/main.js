1e1bc44b7197e5eed6386e5190d55db268fc3130
kohlsjw3656 <kohlsjw3656@gmail.com>
2020-03-05 11:47:33 -0600

    Worked on fixing parallax

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var window, document;
 
 function navResize() {
     var navItems = document.getElementsByClassName("navResize");
-    var vw = document.documentElement.clientWidth;
+    var vw = window.innerWidth;
 
     for (var i = 0; i < navItems.length; i++) {
         if (vw < 768) {
